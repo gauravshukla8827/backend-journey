0891c9cf663fba635726491a7caadf8d63ed7dfa
Gaurav Shukla <gauravshukla8827@gmail.com>
2026-05-28 10:56:09 +0530

    Added Day 3 Treasure Island

---
+++ 3.py
@@ -0,0 +1,47 @@
+"""# if \else statemnent
+print("Welcome to roller coaster")
+height = int(input("height of person\n"))
+
+if height>= 120:
+    print("Ride")
+else:
+    print("cannot ride")
+
+
+    # modolo operator is remainder
+
+num = int(input("enterv the number\n"))
+if num % 2 == 0:
+    print("even")
+else:"""
+
+
+
+"""
+print("welcome to python pizza")
+size = input("what sixze of pizza ? S, M, or L").upper()
+sum =0
+if size == "S":
+    print("small")
+    sum = 15
+elif size == "M":
+    print("medium")
+    sum = 20
+else:
+    print("large")
+    sum = 25
+pep = input("do you wamt peproni? Y or N").upper()
+if pep =="Y":
+    if size == "S":
+        sum += 2
+    else:
+        sum += 3
+cheese = input("do you want cheese? Y or N").upper()
+if cheese == "Y":
+    sum += 1
+print(sum)   
+"""
+    
+# the sum if in the if statement then it would only rum cheese == y
+# if age>= 45 and age <= 55:
+# if age 45<=age<=55:
